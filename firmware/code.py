48030c2d394671235afa324bf8e6973716b56b2c
zortness <zortness@gmail.com>
2023-05-19 20:38:00 -0700

    updating keyboard firmware
    Working on issues/1 ; Toggle for mouse functions. Adding ALT key on power button for normal layers.

--- firmware/code.py
+++ firmware/code.py
@@ -8,6 +8,7 @@ from kmk.scanners import DiodeOrientation
 from kmk.modules.layers import Layers
 from kmk.handlers.sequences import unicode_codepoint_sequence
 from kmk.handlers.sequences import send_string
+from kmk.handlers.sequences import simple_key_sequence
 from kmk.modules.mouse_keys import MouseKeys
 from kmk.extensions.media_keys import MediaKeys
 
@@ -23,24 +24,33 @@ keyboard.diode_orientation = DiodeOrientation.COL2ROW
 
 # custom keys
 ______ = KC.NO
-KC_SQRT = unicode_codepoint_sequence(['221a']) # square root symbol
-KC_PI = unicode_codepoint_sequence(['03c0']) # pi symbol
-KC_SUM = unicode_codepoint_sequence(['2140']) # summation symbol
-KC_INF = unicode_codepoint_sequence(['221e']) # infinity
-KC_INT = unicode_codepoint_sequence(['222b']) # integral
-KC_EX = unicode_codepoint_sequence(['0065','1d61']) # e^x
+KC_SQRT = unicode_codepoint_sequence(["221a"]) # square root symbol
+KC_PI = unicode_codepoint_sequence(["03c0"]) # pi symbol
+KC_SUM = unicode_codepoint_sequence(["2140"]) # summation symbol
+KC_INF = unicode_codepoint_sequence(["221e"]) # infinity
+KC_INT = unicode_codepoint_sequence(["222b"]) # integral
+KC_EX = unicode_codepoint_sequence(["0065","1d61"]) # e^x
 KC_LN = send_string("LN") # LN
 KC_SIN = send_string("SIN") # SIN
 KC_COS = send_string("COS") # COS
 KC_TAN = send_string("TAN") # TAN
-KC_THTA = unicode_codepoint_sequence(['03b8']) # theta
-KC_PWR = unicode_codepoint_sequence(['23fb']) # power
+KC_THTA = unicode_codepoint_sequence(["03b8"]) # theta
+KC_PWR = unicode_codepoint_sequence(["23fb"]) # power
+KC_NXT = simple_key_sequence(
+    (
+        KC.LALT(no_release=True), 
+        KC.MACRO_SLEEP_MS(30),
+        KC.TAB,
+        KC.MACRO_SLEEP_MS(30),
+        KC.LALT(no_press=True),
+    )
+)
 
 # layer keys
 MOUSE_EN = KC.MO(1)
+MOUSE_TG = KC.TG(1)
 SEC_EN = KC.MO(2)
 DIA_EN = KC.MO(3)
-
 
 
 keyboard.keymap = [
@@ -53,7 +63,7 @@ keyboard.keymap = [
         KC.Q,    KC.W,     KC.E,     KC.R,    KC.T,    KC.Y,     KC.U,    KC.I,    KC.O,     KC.P,
         KC.A,    KC.S,     KC.D,     KC.F,    KC.G,    KC.H,     KC.J,    KC.K,    KC.L,     ______,
         KC.LSFT, KC.Z,     KC.X,     KC.C,    KC.V,    KC.B,     KC.N,    KC.M,    KC_THTA,  KC.ENT,
-        KC_PWR,  DIA_EN,   SEC_EN,   KC.TAB,  KC.SPC,  ______,   KC.EQL,  KC.BSPC, ______,   ______,
+        KC.LALT,  DIA_EN,   SEC_EN,   KC.TAB,  KC.SPC,  ______,   KC.EQL,  KC.BSPC, ______,   ______,
     ],
     [ # Layer 1 (mouse)
         ______,  ______,   SEC_EN,   KC.ESC,    KC.MS_UP,  KC.MS_RT, KC.LPRN, KC.RPRN, KC.COMMA, KC.SLASH,
@@ -64,10 +74,10 @@ keyboard.keymap = [
         KC.Q,    KC.W,     KC.E,     KC.R,      KC.T,      KC.Y,     KC.U,    KC.I,    KC.O,     KC.P,
         KC.A,    KC.S,     KC.D,     KC.F,      KC.G,      KC.H,     KC.J,    KC.K,    KC.L,     ______,
         KC.LSFT, KC.Z,     KC.X,     KC.C,      KC.V,      KC.B,     KC.N,    KC.M,    KC_THTA,  KC.ENT,
-        KC_PWR,  DIA_EN,   SEC_EN,   KC.TAB,    KC.SPC,    ______,   KC.EQL,  KC.BSPC, ______,   ______,
+        KC.LALT,  DIA_EN,   SEC_EN,   KC.TAB,    KC.SPC,    ______,   KC.EQL,  KC.BSPC, ______,   ______,
     ],
     [ # Layer 2 (2nd)
-        ______,  MOUSE_EN, SEC_EN,   KC.ESC,  KC.UP,   KC.RIGHT, KC.LCBR, KC.RCBR, KC.LBRC,  KC.RBRC,
+        ______,  MOUSE_TG, SEC_EN,   KC.ESC,  KC.UP,   KC.RIGHT, KC.LCBR, KC.RCBR, KC.LBRC,  KC.RBRC,
         KC.F1,   KC.F5,    KC.LCTRL, ______,  KC.LEFT, KC.DOWN,  KC_INT,  KC.N8,   KC.N9,    KC_SQRT,
         KC.F2,   KC.F6,    KC.DEL,   KC.LGUI, KC.ENT,  ______,   KC_SUM,  KC.N5,   KC.N6,    KC.MINUS,
         KC.F3,   KC.F7,    KC_EX,    ______,  ______,  ______,   KC.N1,   KC.N2,   KC.N3,    KC.PLUS,
@@ -83,10 +93,10 @@ keyboard.keymap = [
         KC.F2,   KC.F6,    KC.DEL,   KC.LGUI, KC.ENT,  ______,   KC.N4,   KC.N5,   KC.N6,    KC.BRID,
         KC.F3,   KC.F7,    KC_LN,    ______,  ______,  ______,   KC.N1,   KC.N2,   KC.N3,    KC.BRIU,
         KC.F4,   KC.F8,    KC_SIN,   KC_COS,  KC_TAN,  KC.CIRC,  KC.N0,   KC.DOT,  KC.MINUS, KC.ENT,
-        KC.Q,    KC.W,     KC.E,     KC.R,    KC.T,    KC.Y,     KC.U,    KC.I,    KC.O,     KC.P,
+        KC.HOME, KC.W,     KC_NXT,   KC.R,    KC.T,    KC.Y,     KC.U,    KC.I,    KC.O,     KC.P,
         KC.A,    KC.S,     KC.D,     KC.F,    KC.G,    KC.H,     KC.J,    KC.K,    KC.L,     ______,
         KC.LSFT, KC.Z,     KC.X,     KC.C,    KC.V,    KC.B,     KC.N,    KC.M,    KC_THTA,  KC.ENT,
-        KC_PWR,  DIA_EN,   SEC_EN,   KC.TAB,  KC.SPC,  ______,   KC.EQL,  KC.DEL,  ______,   ______,
+        KC.LALT,  DIA_EN,   SEC_EN,   KC.TAB,  KC.SPC,  ______,   KC.EQL,  KC.DEL,  ______,   ______,
     ]
 ]
 
